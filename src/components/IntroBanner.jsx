d318e2c8a5903448a9fef2a0f7b1dbb69b6acf46
Duc Hieu Le <duchieule630@gmail.com>
2022-10-24 12:27:47 +0200

    feat: banner text animation

--- src/components/IntroBanner.jsx
+++ src/components/IntroBanner.jsx
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import headerImg from "../assets/astronaut.svg";
 
 const IntroBanner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Front-end Developer", "UI/UX Designer", "iOS Developer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
-    <section className="grid grid-cols-2 gap-2" id="home">
+    <section
+      className="grid grid-cols-2 gap-2 w-full pl-10 sm:pl-28 sm:py-2 rounded"
+      id="home"
+    >
       <div className="block justify-start text-left">
         <h1 className=" font-bold">
-          {`Xin Chao! I'm Harry`} <span>Front-end Developer</span>
+          {`Xin Chao! I'm Harry`} <span>{text}</span>
         </h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
