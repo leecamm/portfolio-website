c16da3bbc86443f0b191237ba50413d029e90c2a
Duc Hieu Le <duchieule630@gmail.com>
2022-10-27 15:11:25 +0200

    feat: skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
+import meter75 from "../assets/75per.svg";
+import meter80 from "../assets/80per.svg";
+import meter90 from "../assets/90per.svg";
+import meter95 from "../assets/95per.svg";
+
 const Skills = () => {
   const responsive = {
     superLargeDesktop: {
@@ -36,36 +41,40 @@ const Skills = () => {
         </div>
         <Carousel responsive={responsive} infinite={true}>
           <div className="">
-            <img
-              className="mx-auto"
-              src="https://img.icons8.com/color/96/000000/react-native.png"
-              alt="react"
-            />
-            <h5 className="">Web Development</h5>
+            <img className="mx-auto w-1/2 h-1/2" src={meter95} alt="react" />
+            <h5 className="pt-5 font-semibold">HTML5/CSS3</h5>
           </div>
           <div className="mx-auto">
-            <img
-              className="mx-auto"
-              src="https://img.icons8.com/color/96/000000/react-native.png"
-              alt="react"
-            />
-            <h5>Web Development</h5>
+            <img className="mx-auto w-1/2 h-1/2" src={meter90} alt="react" />
+            <h5 className="pt-5 font-semibold">ReactJS</h5>
           </div>
           <div className="mx-auto">
-            <img
-              className="mx-auto"
-              src="https://img.icons8.com/color/96/000000/react-native.png"
-              alt="react"
-            />
-            <h5>Web Development</h5>
+            <img className="mx-auto w-1/2 h-1/2" src={meter90} alt="react" />
+            <h5 className="pt-5 font-semibold">TailwindCSS</h5>
           </div>
           <div className="mx-auto">
-            <img
-              className="mx-auto"
-              src="https://img.icons8.com/color/96/000000/react-native.png"
-              alt="react"
-            />
-            <h5>Web Development</h5>
+            <img className="mx-auto w-1/2 h-1/2" src={meter90} alt="react" />
+            <h5 className="pt-5 font-semibold">JavaScript</h5>
+          </div>
+          <div className="mx-auto">
+            <img className="mx-auto w-1/2 h-1/2" src={meter90} alt="react" />
+            <h5 className="pt-5 font-semibold">UI/UX</h5>
+          </div>
+          <div className="">
+            <img className="mx-auto w-1/2 h-1/2" src={meter95} alt="react" />
+            <h5 className="pt-5 font-semibold">Git</h5>
+          </div>
+          <div className="">
+            <img className="mx-auto w-1/2 h-1/2" src={meter95} alt="react" />
+            <h5 className="pt-5 font-semibold">Figma</h5>
+          </div>
+          <div className="mx-auto">
+            <img className="mx-auto w-1/2 h-1/2" src={meter80} alt="react" />
+            <h5 className="pt-5 font-semibold">Unity</h5>
+          </div>
+          <div className="mx-auto">
+            <img className="mx-auto w-1/2 h-1/2" src={meter75} alt="react" />
+            <h5 className="pt-5 font-semibold">Swift/SwiftUI</h5>
           </div>
         </Carousel>
       </div>
