1ed3ddf274b9ed9032ceb887101283f781cdee39
Duc Hieu Le <duchieule630@gmail.com>
2022-11-05 15:09:27 +0100

    fix: contact image

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
 
   return (
     <section
-      className="banner grid grid-cols-1 md:grid-cols-8 lg:grid-cols-10 gap-2 md:gap-6 lg:gap-10 rounded items-center justify-center"
+      className="contact grid grid-cols-1 md:grid-cols-8 lg:grid-cols-10 gap-2 md:gap-6 lg:gap-10 rounded items-center justify-center"
       id="contact"
     >
       <img
